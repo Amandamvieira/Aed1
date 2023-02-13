7657550f3a55982227e8f47aea487f7211c70668
Amanda Vieira <91515414+Amandamvieira@users.noreply.github.com>
2023-02-12 22:45:32 -0300

    exercicio leet code

--- EXERCICIO.C
+++ EXERCICIO.C
@@ -1,27 +1,31 @@
-//Calculadora de potências
+//Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 
-int main() {
-  int x,        /* base                         */ 
-      n,        /* expoente                     */
-      potencia, /* guarda as potencias parciais */ 
-      contador; 
+//You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+// Declaração da função
+int singleNumber(int* nums, int numsSize);
+
+#include <stdio.h>
+
+/* No main criamos um vetor pra passar pra essa função,
+ * e também temos que passar o tamanho do vetor.
+ * nesse caso fiz um vetor com 9 valores e passei 9 pra função.
+ */
+
+int main(){
+
+  int array[] = {1, 1, 2, 2, 3, 3, 4, 4, 5};
+  int result = singleNumber(array, 9); //Result pega o retorno da função
   
-  printf("\n\tCalculo de potencias\n");
-  printf("\nDigite um numero inteiro: ");
-  scanf("%d", &x);
-  printf("Digite um numero um inteiro nao-negativo: ");
-  scanf("%d", &n);
+  printf("A resposta é %d", result); 
   
-  /* Inicializacoes */
-  potencia = 1;
-  contador = 0;
-  
-  /* Calculo da potencia */
-  while (contador ! = n) {
-    potencia = potencia * x;
-    contador = contador + 1;
-  }
-  
-  printf("\nO valor de %d elevado a %d: %d\n", x, n, potencia);
   return 0;
+}
+
+int singleNumber(int* nums, int numsSize){
+    int ans = 0;
+    for(int i = 0; i < numsSize; i++){
+        ans ^= nums[i];
+    }
+    return ans; 
 }
