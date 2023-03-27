6597bcdc8a8b4ff4e9b2d7948ed2bf1155c2805b
Amanda Vieira <91515414+Amandamvieira@users.noreply.github.com>
2023-03-26 22:10:13 -0300

    Update Agenda.c

--- Agenda.c
+++ Agenda.c
@@ -1,165 +1,218 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <windows.h>
 
-#define MAX_NAME_LEN 10
+#define NODE_SIZE 38
+#define PBUFFER_SIZE 22
+#define TEMPNOME 10
 
-// Define a estrutura de uma pessoa
-typedef struct {
-    char nome[MAX_NAME_LEN + 1];
-    int idade;
-    int telefone;
-} Pessoa;
+#define IDADE 10
+#define TELEFONE 14
+#define ANTERIOR 18
+#define PROXIMO 22
 
-// Define a estrutura de um nó da lista duplamente ligada
-typedef struct node {
-    void *data;
-    struct node *next;
-    struct node *prev;
-} Node;
+void *inserir(void *start, void *pBuffer);
+void buscar(void *start);
+void *excluir(void *start, void *pBuffer);
 
-// Cria uma nova pessoa
-Pessoa *criar_pessoa(char *nome, int idade, int telefone) {
-    Pessoa *pessoa = (Pessoa *)malloc(sizeof(Pessoa));
-    if (pessoa == NULL) {
-        printf("Erro ao alocar memória para nova pessoa.\n");
-        exit(1);
-    }
-    strncpy(pessoa->nome, nome, MAX_NAME_LEN);
-    pessoa->nome[MAX_NAME_LEN] = '\0';
-    pessoa->idade = idade;
-    pessoa->telefone = telefone;
-    return pessoa;
-}
+int main()
+{
+    SetConsoleOutputCP(65001);
 
-// Adiciona uma nova pessoa à lista ordenada alfabeticamente
-void adicionar_pessoa(Node **head, Pessoa *nova_pessoa) {
-    // Criar novo nó para a nova pessoa
-    Node *new_node = (Node *)malloc(sizeof(Node));
-    if (new_node == NULL) {
-        printf("Erro ao alocar memória para novo nó.\n");
-        exit(1);
-    }
-    new_node->data = nova_pessoa;
+    void *pBuffer = malloc(PBUFFER_SIZE);
+    void *start = NULL;
 
-    // Encontrar posição na lista para inserir a nova pessoa
-    Node *current_node = (*head)->next;
-    Node *previous_node = *head;
-    while (current_node != NULL && strcasecmp(((Pessoa *)current_node->data)->nome, nova_pessoa->nome) < 0) {
-        previous_node = current_node;
-        current_node = current_node->next;
-    }
+    ((int *)pBuffer)[0] = 0; // switch case
+    ((int *)pBuffer)[1] = 0; // contador de pessoas
 
-    // Inserir novo nó na lista
-    previous_node->next = new_node;
-    new_node->prev = previous_node;
-    new_node->next = current_node;
-    if (current_node != NULL) {
-        current_node->prev = new_node;
-    }
-}
+    for(;;){
 
-// Remove uma pessoa da lista pelo nome
-void remover_pessoa(Node **head, char *nome) {
-    Node *current_node = (*head)->next;
-    while (current_node != NULL) {
-        if (strcasecmp(((Pessoa *)current_node->data)->nome, nome) == 0) {
-            // Remover nó da lista
-            Node *previous_node = current_node->prev;
-            Node *next_node = current_node->next;
-            previous_node->next = next_node;
-            if (next_node != NULL) {
-                next_node->prev = previous_node;
-            }
-            free(current_node->data);
-            free(current_node);
-            return;
+        printf("\n");
+        printf("| 1- Incluir novo cadastro |\n");
+        printf("| 2- Excluir cadastro      |\n");
+        printf("| 3- Buscar                |\n");
+        printf("| 4- Listar                |\n");
+        printf("| 5- Sair                  |\n");
+        scanf("%d", &((int *)pBuffer)[0]);
+        getchar();
+       ////////////////////cases//////////////////////
+
+        switch (((int *)pBuffer)[0])
+        {
+
+        case 1:
+            start = inserir(start, pBuffer);
+            break;
+
+        case 2:
+            start = excluir(start, pBuffer);
+            break;
+
+        case 3:
+            buscar(start);
+            break;
+
+        case 4:
+            listar(start);
+            break;
+
+        case 5:
+            exit(0);
+            break;
+
+        default:
+            break;
         }
-        current_node = current_node->next;
+
+    }  
+
+    free(pBuffer);
+    free(start);
+    return 0;
+}
+//inserir
+void *inserir(void *start, void *pBuffer)
+{
+    void *nodo = malloc(NODE_SIZE);
+
+    printf("\nInsira apenas o primeiro nome do novo usuário: ");
+    scanf("%10s", (char *)nodo);
+    getchar();
+
+    printf("Insira a idade do novo usuário: ");
+    scanf("%d", &*(int *)(nodo + IDADE));
+    printf("Insira o telefone do novo usuário: ");
+    scanf("%d", &*(int *)(nodo + TELEFONE));
+
+    *(void **)(nodo + ANTERIOR) = NULL;
+
+    *(void **)(nodo + PROXIMO) = NULL;
+
+    if (start == NULL)
+    {
+        start = nodo;
+        ((int *)pBuffer)[1]++;
+        return start;
     }
-    printf("Pessoa com nome '%s' não encontrada.\n", nome);
+
+    if ((strcmp((char *)start, (char *)nodo)) >= 0)
+    {
+        *(void **)(nodo + PROXIMO) = start;
+        *(void **)(start + ANTERIOR) = nodo;
+        start = nodo;
+        ((int *)pBuffer)[1]++;
+        return start;
+    }
+
+    void *atual = start;
+    void *anterior = start;
+
+    while (atual != NULL && (strcmp((char *)atual, (char *)nodo) <= 0))
+    {
+        anterior = atual;
+        atual = *(void **)(atual + PROXIMO);
+    }
+
+    *(void **)(anterior + PROXIMO) = nodo;
+    *(void **)(nodo + PROXIMO) = atual;
+    *(void **)(nodo + ANTERIOR) = anterior;
+
+    ((int *)pBuffer)[1]++;
+    return start;
+}
+//excluir
+void *excluir(void *start, void *pBuffer)
+{
+    if (start == NULL)
+    {
+        printf("Nenhum usuário cadastrado.\n");
+        return start;
+    }
+
+    char tempNome[TEMPNOME];
+    printf("Insira o primeiro nome do usuário que deseja excluir: ");
+    scanf("%s", tempNome);
+
+    void *atual = start;
+    void *anterior = start;
+
+    while (atual != NULL)
+    {
+        if (strcmp((char *)atual, tempNome) == 0)
+        {
+            if (atual == start)
+            {
+                start = *(void **)(start + PROXIMO);
+            }
+            else
+            {
+                *(void **)(anterior + PROXIMO) = *(void **)(atual + PROXIMO);
+                if (*(void **)(atual + PROXIMO) != NULL)
+                {
+                    *(void **)(*(void **)(atual + PROXIMO) + ANTERIOR) = anterior;
+                }
+            }
+
+            free(atual);
+            ((int *)pBuffer)[1]--;
+            printf("Usuário excluído com sucesso.\n");
+            return start;
+        }
+
+        anterior = atual;
+        atual = *(void **)(atual + PROXIMO);
+    }
+
+    printf("Usuário não encontrado.\n");
+    return start;
 }
 
-// Busca uma pessoa na lista pelo nome e exibe suas informações
-void buscar_pessoa(Node *head, char *nome) {
-    Node *current_node = head->next;
-    while (current_node != NULL) {
-        if (strcasecmp(((Pessoa *)current_node->data)->nome, nome) == 0) {
-            printf("Nome: %s\n", ((Pessoa *)current_node->data)->nome);
-            printf("Idade: %d\n", ((Pessoa *)current_node->data)->idade);
-            printf("Telefone: %d\n\n",((Pessoa *)current_node->data)->telefone);
+//buscar
+void buscar(void *start)
+{
+    if (start == NULL)
+    {
+        printf("Nenhum usuário cadastrado.\n");
         return;
     }
-    current_node = current_node->next;
-}
-printf("Pessoa com nome '%s' não encontrada.\n", nome);
-}
 
-// Exibe todas as pessoas da lista
-void listar_pessoas(Node *head) {
-Node *current_node = head->next;
-while (current_node != NULL) {
-printf("Nome: %s\n", ((Pessoa *)current_node->data)->nome);
-printf("Idade: %d\n", ((Pessoa *)current_node->data)->idade);
-printf("Telefone: %d\n\n", ((Pessoa *)current_node->data)->telefone);
-current_node = current_node->next;
-}
-}
+    char tempNome[TEMPNOME];
+    printf("Insira o primeiro nome do usuário que deseja buscar: ");
+    scanf("%s", tempNome);
 
-int main() {
-Node *head = (Node *)malloc(sizeof(Node));
-if (head == NULL) {
-printf("Erro ao alocar memória para nó cabeça.\n");
-return 1;
-}
-head->data = NULL;
-head->next = NULL;
-head->prev = NULL;
-int opcao;
-char nome[MAX_NAME_LEN + 1];
-int idade, telefone;
+    void *atual = start;
+    int encontrado = 0;
 
-while (1) {
-    printf("Selecione uma opção:\n");
-    printf("1 - Adicionar pessoa\n");
-    printf("2 - Remover pessoa\n");
-    printf("3 - Buscar pessoa\n");
-    printf("4 - Listar pessoas\n");
-    printf("5 - Sair\n");
-    printf("> ");
-    scanf("%d", &opcao);
+    while (atual != NULL)
+    {
+        if (strcmp((char *)atual, tempNome) == 0)
+        {
+            printf("Nome: %s\n", (char *)atual);
+            printf("Idade: %d\n", *(int *)(atual + IDADE));
+            printf("Telefone: %d\n", *(int *)(atual + TELEFONE));
+            printf("\n");
+            encontrado = 1;
+            break;
+        }
 
-    switch (opcao) {
-        case 1:
-            printf("Digite o nome da pessoa: ");
-            scanf("%s", nome);
-            printf("Digite a idade da pessoa: ");
-            scanf("%d", &idade);
-            printf("Digite o telefone da pessoa: ");
-            scanf("%d", &telefone);
-            adicionar_pessoa(&head, criar_pessoa(nome, idade, telefone));
-            break;
-        case 2:
-            printf("Digite o nome da pessoa a ser removida: ");
-            scanf("%s", nome);
-            remover_pessoa(&head, nome);
-            break;
-        case 3:
-            printf("Digite o nome da pessoa a ser buscada: ");
-            scanf("%s", nome);
-            buscar_pessoa(head, nome);
-            break;
-        case 4:
-            listar_pessoas(head);
-            break;
-        case 5:
-            printf("Saindo...\n");
-            return 0;
-        default:
-            printf("Opção inválida.\n");
-            break;
+        atual = *(void **)(atual + PROXIMO);
+    }
+
+    if (!encontrado)
+    {
+        printf("Usuário não encontrado.\n");
     }
 }
 
-return 0;
+//listar
+void listar(void *start) {
+    void *atual = start;
+    while (atual != NULL) {
+        printf("Nome: %s\n", (char *)atual);
+        printf("Idade: %d\n", *(int *)(atual + IDADE));
+        printf("Telefone: %d\n", *(int *)(atual + TELEFONE));
+        printf("\n");
+        atual = *(void **)(atual + PROXIMO);
+    }
 }
