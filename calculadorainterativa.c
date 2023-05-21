dd5efc6cc26657d1a17a90a041677551c5e86edf
Amanda Vieira <91515414+Amandamvieira@users.noreply.github.com>
2023-05-21 20:22:23 -0300

    Add files via upload

---
+++ calculadorainterativa.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2, resultado;
+    char operador;
+
+    printf("Digite o primeiro número: ");
+    scanf("%f", &num1);
+
+    printf("Digite o segundo número: ");
+    scanf("%f", &num2);
+
+    printf("Digite a operação desejada (+, -, *, /): ");
+    scanf(" %c", &operador);
+
+    switch (operador) {
+        case '+':
+            resultado = num1 + num2;
+            printf("Resultado: %.2f\n", resultado);
+            break;
+        case '-':
+            resultado = num1 - num2;
+            printf("Resultado: %.2f\n", resultado);
+            break;
+        case '*':
+            resultado = num1 * num2;
+            printf("Resultado: %.2f\n", resultado);
+            break;
+        case '/':
+            if (num2 != 0) {
+                resultado = num1 / num2;
+                printf("Resultado: %.2f\n", resultado);
+            } else {
+                printf("Erro: divisão por zero não é permitida.\n");
+            }
+            break;
+        default:
+            printf("Operador inválido.\n");
+    }
+
+    return 0;
+}
